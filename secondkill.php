33b7bfd63a8961b3524417e07d9de64d64fc40c6
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-03 21:36:49 +0800

    Create secondkill.php

---
+++ secondkill.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<body>
+<form action=secondkill_result.php method=post>
+用户姓名 ：<input type="text" name="username" /><br/>
+手机后4位：<input type="text" name="code" /><br/>
+<input type="submit" value="秒杀"/>
+</form>
+</body>
+</html>
