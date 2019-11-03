646887ad8883864171ef789c473608e98fab0839
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-03 22:15:47 +0800

    Update secondkill_result.php

--- secondkill_result.php
+++ secondkill_result.php
@@ -9,11 +9,11 @@ $code = trim($_POST['code']);
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
 $time=100000000*microtime();
 $sql = "insert into secondkill values('".$username."-".$code."',".$time.")";
-echo $sql;
+
 if ($connection->query($sql) !== TRUE) {  
 	echo "发生数据库操作错误或用户".$username."已经存在";
 } else {
-	echo "抽奖用户注册成功。";
+	echo "你使用以下信息参加本轮秒杀：";
 	echo "<br/>用户名：".$username;
 	echo "<br/>手机后4位：".$code;
 }
