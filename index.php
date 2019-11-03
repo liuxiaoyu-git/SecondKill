93cb66b225bbc2427033651878c7a11c97b86ecb
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-03 21:08:10 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        <table width="100%" height="100%">
+            <tr>
+                <td align="center">
+                    <a href="seckill.php">1）去秒杀</a><br/>
+                    <a href="showwinner.php">2）显示秒杀结果</a><br/>
+                    <a href="initapp.php">3）重置秒杀应用（清除所有数据）</a>
+                </td>
+	        </tr>
+        </table>
+    </body>
+</html>
